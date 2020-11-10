268f6340a93a7e60e1a702c2fe81fdf23dbd87c6
Stephen Jackson <jjackson19862017@gmail.com>
2020-11-10 00:08:35 +0000

    Improving OOP Database Connection

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -4,27 +4,34 @@
 		public $connection; // Makes this variable public
 		// Runs automatically to connect to the database.
 		function __construct(){
-			$this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-			if(mysqli_connect_errno()) {
-				die("Database not connected" . mysqli_error());
+			$this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+			if($this->connection->connect_errno) {
+				die("Database not connected" . $this->connection->connect_error);
 			}
 		}
 		// Runs a query to Mysql Databse
 		public function query($sql) {
-			$result = mysqli_query($this->connection, $sql);
+			$result = $this->connection->query($sql);
+			$this->confirm_query($result);
 			return $result;		
 		}
 		// Confirms that the query has been successful
 		public function confirm_query($result) {
 			if(!$result){
-				die("Query Failed");
+				die("Query Failed " . $this->connection->error);
 			}			
 		}
 		// Stops hackers from injecting code into our Database
 		public function escape_string($string){
-			$escaped_string = mysqli_real_escape_string($this->connection, $string);
+			$escaped_string = $this->connection->real_escape_string($string);
 			return $escaped_string;
 		}
+		// Insert Id
+		public function the_insert_id(){
+			return $this->connection->insert_id;
+		}
+
+
 }
 // Creates a instance of the database
 $database = new Database();
