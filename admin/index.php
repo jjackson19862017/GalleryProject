eb75f94adccaa0bf3be38c7a4e9c337c002cc749
Stephen Jackson <jjackson19862017@gmail.com>
2020-11-10 01:14:23 +0000

    Using Instantiation Method to find all users

--- admin/index.php
+++ admin/index.php
@@ -27,15 +27,20 @@
                         
                         <?php 
                             // Runs a query to return a result
-                            $result_set = User::find_all_users();
-                            while($row = mysqli_fetch_array($result_set)) {
-                                echo $row['username'] . "<br>";
-                                }
+                            // $result_set = User::find_all_users();
+                            // while($row = mysqli_fetch_array($result_set)) {
+                            //     echo $row['username'] . "<br>";
+                            //     }
+
+
+                            $users = User::find_all_users();
+                            foreach($users as $user) {
+                                echo $user->id . " " . $user->username . "<br>";
+                            }
 	                        // Challenge to Find User By Id
                             /*    $found_user = User::find_user_by_id(1);
                                 $user = User::instantation($found_user);
                                 echo $user->username;*/
-
 	                        
 	                        
                         ?>
