4a0a21df68f1f4c2a00bfc09b1aa9ad957ea1a8c
Stephen Jackson <jjackson19862017@gmail.com>
2020-11-10 14:10:36 +0000

    Creating The Login Page

---
+++ admin/includes/login.php
@@ -0,0 +1,43 @@
+<?php require_once("init.php"); ?>
+
+<?php
+
+// If user is signed in goto Admin Index Page
+if($session->is_signed_in()) { redirect("index.php"); }
+
+if(isset($_POST['submit'])) {
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+// Method to check database user
+
+
+
+    if($user_found) {
+        $session->login($user_found);
+        redirect("index.php");
+    } else {
+        $the_message = "Your Password or Username are incorrect.";
+    }
+
+} else {
+    // If the Fields are blank, then set the variables to avoid an error.
+    $username = "";
+    $password = "";
+}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+?>
