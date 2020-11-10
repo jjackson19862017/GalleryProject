df47ad4b24e747aa8b007befa9d239a09051c481
Stephen Jackson <jjackson19862017@gmail.com>
2020-11-10 13:28:49 +0000

    The Login Method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,14 +2,28 @@
 
 class Session {
 
-    private $signed_in;
+    private $signed_in = false;
     public $user_id;
 
     // Auto starts a session each time its run
     function __construct(){
         session_start();
+        $this->check_the_login();
     }
 
+    // Returns a True or False value depending on if the user is logged in or not
+    public function is_signed_in() {
+        return $this->signed_in;
+    }
+
+    public function login($user) {
+        if($user) {
+            $this->user_id = $_SESSION['user_id'] = $user->id; //Assigns 2 values to the variable.
+            $this->signed_in = true;
+        }
+    }
+
+    // If True Sets the user_id property
     private function check_the_login() {
         if(isset($_SESSION['user_id'])) {
             $this->user_id = $_SESSION['user_id'];
