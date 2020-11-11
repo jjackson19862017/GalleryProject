52ce2fe81ac08a55e16886a90fec21da02000d1f
Stephen Jackson <jjackson19862017@gmail.com>
2020-11-11 23:38:56 +0000

    Coding the Php for the Photo Table

--- admin/photos.php
+++ admin/photos.php
@@ -1,5 +1,7 @@
 <?php include("includes/header.php"); ?>
 
+<?php if(!$session->is_signed_in()) { redirect("login.php");} // Checks to see if the user is logged in ?>
+
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -24,14 +26,34 @@
                             Photos
                             <small>Subheading</small>
                         </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
+                        <div class="col-md-12">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Photo</th>
+                                    <th>Id</th>
+                                    <th>Filename</th>
+                                    <th>Title</th>
+                                    <th>Size</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                
+                                    <?php 
+                                    
+                                    $photos = Photo::find_all();
+                                    foreach ($photos as $photo) : ?>
+                                    <tr>
+                                    <td><img style="height: 100px;" class="img-thumbnail" src="http://localhost/gallery/admin/images/<?php echo $photo->filename; ?>" alt=""></td>
+                                    <td><?php echo $photo->photo_id; ?></td>
+                                    <td><?php echo $photo->filename; ?></td>
+                                    <td><?php echo $photo->title; ?></td>
+                                    <td><?php echo $photo->size; ?></td>
+                                    </tr>
+                                    <?php endforeach; ?>
+                            </tbody>
+                        </table> <!-- End of Table -->                        
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
