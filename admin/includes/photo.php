6d1653a7a18a2f9359bc40bd5b1bf2aabd8fcbef
Stephen Jackson <jjackson19862017@gmail.com>
2020-11-11 10:43:32 +0000

    Setting up our Properties Array

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -11,5 +11,23 @@ class Photo extends Db_object {
     public $type;
     public $size;
 
+    public $tmp_path;
+    public $upload_directory = "images";
+    public $custom_errors = array();
+    public $upload_errors_array = array(
+        UPLOAD_ERR_OK          =>"there is no error",
+        UPLOAD_ERR_INI_SIZE    =>"there is erron in ini size",
+        UPLOAD_ERR_FORM_SIZE   =>"there is error in form size",
+        UPLOAD_ERR_PARTIAL     =>"the uploaded file was oly partially uploaded",
+        UPLOAD_ERR_NO_FILE     =>"there is no file was uploaded",
+        UPLOAD_ERR_NO_TMP_DIR  =>"there is missing a temporary folder ",
+        UPLOAD_ERR_CANT_WRITE  =>"filed to write file to disk",
+        UPLOAD_ERR_EXTENSION   =>"a php extension stopped the file upload"
+      );
+      
+
+
+
+
 } // End of Photo Class
 ?>
