846b88500ceb0eb89b66e03784b9de51836e159f
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-11-14 00:47:17 +0000

    Picture and Sidebar Styling

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -73,31 +73,35 @@
                                     <h4>Save <span id="toggle" class="glyphicon glyphicon-menu-up pull-right"></span></h4>
                                     </div>
                                 <div class="inside">
-                                <div class="box-inner">
-                                    <p class="text">
-                                    <span class="glyphicon glyphicon-calendar"></span> Uploaded on: April 22, 2030 @ 5:26
-                                    </p>
-                                    <p class="text ">
-                                        Photo Id: <span class="data photo_id_box"><?php echo $photo->id ; ?></span>
-                                    </p>
-                                    <p class="text">
-                                        Filename: <span class="data"><?php echo $photo->filename ; ?></span>
-                                    </p>
-                                    <p class="text">
-                                    File Type: <span class="data"><?php echo $photo->type ; ?></span>
-                                    </p>
-                                    <p class="text">
-                                    File Size: <span class="data"><?php echo $photo->size ; ?></span>
-                                    </p>
-                                </div>
-                                <div class="info-box-footer clearfix">
-                                    <div class="info-box-delete pull-left">
-                                        <a  href="delete_photo.php?id=<?php echo $photo->id; ?>" class="btn btn-danger btn-lg ">Delete</a>   
+                                    <div class="box-inner">
+                                        <p class="text">
+                                        <span class="glyphicon glyphicon-calendar"></span> Uploaded on: April 22, 2030 @ 5:26
+                                        </p>
+                                        <p class="text ">
+                                            Photo Id: <span class="data photo_id_box"><?php echo $photo->id ; ?></span>
+                                        </p>
+                                        <p class="text">
+                                            Filename: <span class="data"><?php echo $photo->filename ; ?></span>
+                                        </p>
+                                        <p class="text">
+                                        File Type: <span class="data"><?php echo $photo->type ; ?></span>
+                                        </p>
+                                        <p class="text">
+                                        File Size: <span class="data"><?php echo $photo->size ; ?></span>
+                                        </p>
                                     </div>
-                                    <div class="info-box-update pull-right ">
-                                        <input type="submit" name="update" value="Update" class="btn btn-primary btn-lg ">
-                                    </div>   
-                                </div>
+                                    <div class="info-box-footer clearfix">
+                                        <div class="info-box-delete pull-left">
+                                            <a  href="delete_photo.php?id=<?php echo $photo->id; ?>" class="btn btn-danger btn-lg ">Delete</a>   
+                                        </div>
+                                        <div class="info-box-update pull-right ">
+                                            <input type="submit" name="update" value="Update" class="btn btn-primary btn-lg ">
+                                        </div>   
+                                    </div>
+                                    <hr>
+                                    <div class="img-thumbnail img-fluid">
+                                    <a href=""><img class="" src="<?php echo $photo->picture_path(); ?>" alt="<?php echo $photo->alt_text ; ?>"></a>
+                                    </div>
                                 </div>          
                             </div>
 
