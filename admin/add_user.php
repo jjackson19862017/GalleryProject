522ce9856e4ea61193750c8562c49a8040576f24
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-11-14 16:22:10 +0000

    Duplication Bug Fixed

--- admin/add_user.php
+++ admin/add_user.php
@@ -54,11 +54,11 @@
                                 </div>
                                 <div class="form-group">
                                     <label for="username">Username</label>
-                                    <input type="text" name="username" class="form-control" id="">
+                                    <input type="text" name="username" class="form-control" id="" autocomplete="off">
                                 </div>
                                 <div class="form-group">
                                     <label for="password">Password</label>
-                                    <input type="password" name="password" class="form-control" id="">
+                                    <input type="password" name="password" class="form-control" id="" autocomplete="off">
                                 </div>
                                 <div class="form-group">
                                     <label for="first_name">First Name</label>
--- admin/edit_user.php
+++ admin/edit_user.php
@@ -22,7 +22,7 @@
             $user->save();                
             } else {
                 $user->set_file($_FILES['user_image']);
-                $user->save_user_and_image();
+                $user->upload_photo();
                 $user->save();  
                 
                 redirect("edit_user.php?id={$user->id}");
