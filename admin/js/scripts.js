52c5c9d840a656749b11958e44b6bae9c9bef863
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-11-15 22:28:06 +0000

    Writing the AJAX - Setting User Image

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -6,27 +6,52 @@ $(document).ready(function(){
 
     var image_source;
     var image_href_splitted;
-    var image_id;
+    var image_name;
     
     $(".modal_thumbnails").click(function(){
 
         $("#set_user_image").prop('disabled', false); // Enables the button on the modal
 
         // Gets the User Id
-        user_source = $("#user-id").prop('href');
-        user_href_splitted = user_source.split("="); // Splits the string at the = 
+        $(this).addClass('selected');
+        user_href = $("#user-id").prop('href');
+        user_href_splitted = user_href.split("="); // Splits the string at the = 
         user_id = user_href_splitted[user_href_splitted.length -1]; // Finds the length of the array
         // Test alert(user_id);
 
         // Gets the Image Id
-        image_href = $(this).prop('src');
-        image_href_splitted = image_href.split("/"); // Splits the string at the /
-        image_id = image_href_splitted[image_href_splitted.length -1]; // Finds the length of the array
-        // Test alert(image_id);
+        image_source = $(this).prop('src');
+        image_href_splitted = image_source.split("/"); // Splits the string at the /
+        image_name = image_href_splitted[image_href_splitted.length -1]; // Finds the length of the array
+        // Test alert(image_name);
 
 
     });
 
+    $("#set_user_image").click(function(){ 
+
+        $.ajax({
+
+            url: "includes/ajax_code.php",
+            data:{image_name: image_name, user_id:user_id}, // Setting Objects
+            type:"POST", // Catching the post super global
+            success:function(data){
+                if(!data.error) {
+                    alert(image_name);
+                }
+
+
+            }
+
+        });
+
+
+
+
+
+
+
+    });
 
 
 
