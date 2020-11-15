ea0fe9c1c4e6872fd807e76a6d1b55419e66ef05
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-11-15 19:39:09 +0000

    Setting Up Next Page Link and Previous Link

--- index.php
+++ index.php
@@ -30,4 +30,23 @@ $photos = Photo::find_by_query($sql);
             </div> <!-- /.col-md-12 -->
         </div> <!-- /.row -->
 
+        <div class="row">
+        <ul class="pager">
+            <?php 
+            if($paginate->page_total() > 1) {
+                if($paginate->has_next()){
+                    echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
+                }
+                if($paginate->has_previous()){
+                    echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
+                }
+            }
+            ?>
+
+
+            
+
+        </ul>
+        
+        </div>
         <?php include("includes/footer.php"); ?>
