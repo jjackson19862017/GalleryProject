ca394547e9bc2beb755c1deb4ec3c1b79b944676
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-11-15 19:15:23 +0000

    Instantiating and Testing - Error

--- admin/includes/paginate.php
+++ admin/includes/paginate.php
@@ -6,10 +6,10 @@ class Paginate {
     public $items_total_count;
 
 
-    public function __construct($page=1, $items_per_page=4, $items_total_count=0){
-        $this->$current_page =(int)$page;
-        $this->$items_per_page =(int)$items_per_page;
-        $this->$items_total_count =(int)$items_total_count;
+    public function __construct($page, $items_per_page, $items_total_count){
+        $this->current_page =(int)$page;
+        $this->items_per_page =(int)$items_per_page;
+        $this->items_total_count =(int)$items_total_count;
 
     } // End of construct
 
--- index.php
+++ index.php
@@ -6,7 +6,14 @@
 $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1; // If not set, then its 1
 $items_per_page = 4;
 $items_total_count = Photo::count_all();
-$photos = Photo::find_all();
+
+
+$paginate = new Paginate($page, $items_per_page, $items_total_count);
+$sql = "SELECT * FROM photos ";
+$sql.= "LIMIT {$items_per_page} ";
+$sql.= "OFFSET {$paginate->offset()}";
+$photos = Photo::find_by_query($sql);
+
 ?>
 
 <?php $photos = Photo::find_all() ?>
