94000a2f74ec21a45a894435b8efde822d776830
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-11-15 13:37:51 +0000

    Setting up Gallery to Display Photos

--- index.php
+++ index.php
@@ -1,30 +1,18 @@
 <?php include("includes/header.php"); ?>
 
-
+<?php $photos = Photo::find_all() ?>
         <div class="row">
-
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
-
-    
-            
-          
-         
-
-            </div>
-
-
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
-        </div>
-        <!-- /.row -->
+            <div class="col-md-12">
+                <div class="thumbnails row">
+                    <?php foreach ($photos as $photo):?>
+                        <div class="col-xs-6 col-md-3">
+                            <a class="thumbnail" href="">
+                            <img src="admin/<?php echo $photo->picture_path();?>" alt="">
+                            </a>
+                        </div> <!-- /.col-xs-6 col-md-3 -->
+                    <?php endforeach; ?>
+                </div> <!-- /.thumbnails row -->
+            </div> <!-- /.col-md-12 -->
+        </div> <!-- /.row -->
 
         <?php include("includes/footer.php"); ?>
