80b697ad1ec64a427256b1fe6539768521945da0
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-11-15 22:20:11 +0000

    Pulling Image Name with Jquery and Javascript

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -3,18 +3,26 @@ $(document).ready(function(){
     var user_href;
     var user_href_splitted;
     var user_id;
+
+    var image_source;
+    var image_href_splitted;
+    var image_id;
     
     $(".modal_thumbnails").click(function(){
 
         $("#set_user_image").prop('disabled', false); // Enables the button on the modal
 
         // Gets the User Id
-        user_href = $("#user-id").prop('href');
-        user_href_splitted = user_href.split("="); // Splits the string at the = 
+        user_source = $("#user-id").prop('href');
+        user_href_splitted = user_source.split("="); // Splits the string at the = 
         user_id = user_href_splitted[user_href_splitted.length -1]; // Finds the length of the array
-        alert(user_id);
+        // Test alert(user_id);
 
-
+        // Gets the Image Id
+        image_href = $(this).prop('src');
+        image_href_splitted = image_href.split("/"); // Splits the string at the /
+        image_id = image_href_splitted[image_href_splitted.length -1]; // Finds the length of the array
+        // Test alert(image_id);
 
 
     });
